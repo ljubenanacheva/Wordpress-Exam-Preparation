67874b3dc95b1d53deb303178998f0190f8fd284
Lyubena Nacheva <32265269+ljubenanacheva@users.noreply.github.com>
2023-04-28 11:34:11 +0300

    Added header and footer.

--- wp-content/themes/sotuni-jobs/index.php
+++ wp-content/themes/sotuni-jobs/index.php
@@ -1,20 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Jobs</title>
-	<link rel="preconnect" href="https://fonts.gstatic.com">
-
-	<link rel="stylesheet" href="wp-content\themes\sotuni-jobs\assets\css\master.css">
-	<link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap" rel="stylesheet">
-</head>
-<body>
-	<div class="site-wrapper">
-		<header class="site-header">
-			<h1 class="site-title"><a href="#">Job Offers</a></h1>
-		</header>
+<?php get_header(); ?>
 
 		<ul class="jobs-listing">
 			<li class="job-card">
@@ -56,14 +40,4 @@
 			</li>
 		</ul>
 		
-		<footer class="site-footer">
-			<p>Copyright 2020 | Developer links: 
-				<a href="/edits.html">Edits</a>,
-				<a href="/index.html">Home</a>,
-				<a href="/single.html">Single</a>
-			</p>
-		</footer>
-	</div>
-
-</body>
-</html>
+		<?php get_footer(); ?>
