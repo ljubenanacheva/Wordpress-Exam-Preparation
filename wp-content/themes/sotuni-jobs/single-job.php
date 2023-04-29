3e202975279179fe54d83a49bf03512d6e313a93
Lyubena Nacheva <32265269+ljubenanacheva@users.noreply.github.com>
2023-04-29 11:45:09 +0300

    Update single-job view with the right template.

--- wp-content/themes/sotuni-jobs/single-job.php
+++ wp-content/themes/sotuni-jobs/single-job.php
@@ -6,12 +6,12 @@
 
 	<?php while( have_posts() ) : the_post(); ?>
 
-		<?php get_template_part( 'template-parts/single-post', 'item' ); ?>
+		<?php get_template_part( 'template-parts/single-job', 'item' ); ?>
 
 	<?php endwhile; ?>
 
 	<?php softuni_update_job_views_count( get_the_ID() ); ?>
-	
+
 <?php endif; ?>		
 		
 </ul>
